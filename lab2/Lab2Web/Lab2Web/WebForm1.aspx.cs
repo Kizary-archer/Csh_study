07411916f1b3f108ef15561ff34f5228faf144be
Unknown <Kizary-archer@yandex.ru>
2019-03-17 23:44:53 +0300

    add Web

---
+++ lab2/Lab2Web/Lab2Web/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+
+namespace Lab2Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["warehouseConnectionString"].ConnectionString;
+            SqlConnection сonnectWarehousebd = new SqlConnection(connectionString);
+
+            сonnectWarehousebd.Open();
+            TextBox1.Text = String.Format("Версия сервера:{0} \n", сonnectWarehousebd.ServerVersion);
+            TextBox1.Text += String.Format("Состояние соединения:{0} \n", сonnectWarehousebd.State.ToString());
+            сonnectWarehousebd.Close();
+            TextBox1.Text += String.Format("Состояние соединения:{0} \n ", сonnectWarehousebd.State.ToString());
+
+        }
+    }
+}
